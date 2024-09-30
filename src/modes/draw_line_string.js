96dca33fcc60a0c5d6b5d26d4bd9c2dd321a372f
Jay Borseth <jayborseth@hotmail.com>
2024-09-30 11:59:31 -0700

    make tap debounce 500mS

--- src/modes/draw_line_string.js
+++ src/modes/draw_line_string.js
@@ -103,7 +103,7 @@ DrawLineString.lastTapTime = Date.now();
 
 DrawLineString.onTap = function (state, e) {
   const now = Date.now();
-  const tapDebounceTimeMS = 300;
+  const tapDebounceTimeMS = 500;
   if (now - this.lastTapTime < tapDebounceTimeMS) {
     return;
   }
